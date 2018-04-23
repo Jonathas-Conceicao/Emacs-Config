4c1c5ca2d0e0f2818d2aa36d0794c77f07e7f398
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-04-22 23:03:14 -0300

    Git Ignore

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -139,3 +139,17 @@
 (setq wakatime-api-key "38377444-781c-4473-994e-1a3fca37ac3c")
 (global-wakatime-mode)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+	 (quote
+		(markdown-mode wakatime-mode powerline highlight-symbol haskell-mode go-mode git-gutter flyspell-correct atom-one-dark-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
