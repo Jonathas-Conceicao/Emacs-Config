2546ffba83bce2f5e356110a58c6e196a02183ce
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-06-17 17:04:14 -0300

    Guards for package load and Wakatime mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -110,26 +110,30 @@
 (set-keyboard-coding-system 'utf-8)
 (require 'iso-transl)
 
-(require 'whitespace)
-;; Config for whitespace-mode
-;; Make whitespace-mode with very basic background coloring for whitespaces.
-;; Edited from: http://ergoemacs.org/emacs/whitespace-mode.html
-(setq whitespace-style (quote (face spaces tabs newline space-mark tab-mark newline-mark )))
-;; Make whitespace-mode and whitespace-newline-mode use “¬” for end of line char and “»” for tab.
-(setq whitespace-display-mappings
-;; all numbers are unicode codepoint in decimal. e.g. (insert-char 182 1)
-			'((space-mark 32 [183] [46]) ; SPACE 32 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
-				(newline-mark 10 [172 10]) ; LINE FEED,
-				(tab-mark 9 [124 9] [124 9]) ; tab
-				))
-(set-face-attribute 'whitespace-space nil :background nil :foreground "gray40")
-(set-face-attribute 'whitespace-tab nil :background nil :foreground "gray40")
-(set-face-attribute 'whitespace-newline nil :background nil :foreground "gray40")
-(global-whitespace-mode 1)
+(when (require 'whitespace nil :noerror)
+	;; Config for whitespace-mode
+	;; Make whitespace-mode with very basic background coloring for whitespaces.
+	;; Edited from: http://ergoemacs.org/emacs/whitespace-mode.html
+	(setq whitespace-style (quote (face spaces tabs newline space-mark tab-mark newline-mark )))
+	;; Make whitespace-mode and whitespace-newline-mode use “¬” for end of line char and “»” for tab.
+	(setq whitespace-display-mappings
+				;; all numbers are unicode codepoint in decimal. e.g. (insert-char 182 1)
+				'((space-mark 32 [183] [46]) ; SPACE 32 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+					(newline-mark 10 [172 10]) ; LINE FEED,
+					(tab-mark 9 [124 9] [124 9]) ; tab
+					)
+				)
+	(set-face-attribute 'whitespace-space nil :background nil :foreground "gray40")
+	(set-face-attribute 'whitespace-tab nil :background nil :foreground "gray40")
+	(set-face-attribute 'whitespace-newline nil :background nil :foreground "gray40")
+	(global-whitespace-mode 1)
+	)
 
 ;; The default theme
-(require 'dracula-theme)
-(load-theme 'dracula t)
+
+(when (require 'dracula-theme nil :noerror)
+	(load-theme 'dracula t)
+	)
 
 ;; Golang configs
 ;; (require 'go-autocomplete)
@@ -140,54 +144,62 @@
 (add-hook 'before-save-hook #'gofmt-before-save)
 
 ;; Setup for highlight-symbol
-(require 'highlight-symbol)
-(global-set-key [f3] 'highlight-symbol)
-(global-set-key [(control f3)] 'highlight-symbol-next)
-(global-set-key [(shift f3)] 'highlight-symbol-prev)
-(global-set-key [(meta f3)] 'highlight-symbol-query-replace)
+(when (require 'highlight-symbol nil :noerror)
+	(global-set-key [f3] 'highlight-symbol)
+	(global-set-key [(control f3)] 'highlight-symbol-next)
+	(global-set-key [(shift f3)] 'highlight-symbol-prev)
+	(global-set-key [(meta f3)] 'highlight-symbol-query-replace)
+	)
 
 ;; Enables flyspell
-(require 'flyspell)
+(when (require 'flyspell nil :noerror)
+	;; Avoid flyspell slowdown
+	(setq flyspell-issue-message-flag nil)
 
-;; Avoid flyspell slowdown
-(setq flyspell-issue-message-flag nil)
-
-;; Flyspell for latex
-(add-hook 'LaTeX-mode-hook #'turn-on-flyspell)
+	;; Flyspell for latex
+	(add-hook 'LaTeX-mode-hook #'turn-on-flyspell)
+	)
 
 ;; Enables auto complete
 ;; (ac-config-default)
 
 ;; Enables powerline with default profile
-(require 'powerline)
-(powerline-default-theme)
+(when (require 'powerline nil :noerror)
+	(powerline-default-theme)
+	)
 
-(require 'git-gutter)
-(global-git-gutter-mode +1)
+(when (require 'git-gutter nil :noerror)
+	(global-git-gutter-mode +1)
+	)
 
 ;; Emacs Statistic mode
-(require 'ess-site)
+(when (require 'ess-site nil :noerror)
 
-;; Change code style
-(setq ess-default-style 'GNU)
-;; To go back to default style
-;; (setq ess-default-style 'DEFAULT)
+	;; Change code style
+	(setq ess-default-style 'GNU)
+
+	;; To go back to default style
+	;; (setq ess-default-style 'DEFAULT)
+	)
 
 ;; Manually installed packages
 ;; godot-gdscript
 (add-to-list 'load-path "~/.emacs.d/packages-extras/godot-gdscript.el/")
-(require 'godot-gdscript)
-(add-hook 'godot-gdscript-mode-hook
- (lambda ()
-	 (setq-local godot-gdscript-indent-guess-indent-offset nil)
-	 (setq-local godot-gdscript-indent-offset 4)
-	 (setq-local tab-width 4)
-	 (setq-local indent-tabs-mode t)
-	 )
- )
+(when (require 'godot-gdscript nil :noerror)
+	(add-hook 'godot-gdscript-mode-hook
+						(lambda ()
+							(setq-local godot-gdscript-indent-guess-indent-offset nil)
+							(setq-local godot-gdscript-indent-offset 4)
+							(setq-local tab-width 4)
+							(setq-local indent-tabs-mode t)
+							)
+						)
+	)
 
-(setq wakatime-api-key "38377444-781c-4473-994e-1a3fca37ac3c")
-(global-wakatime-mode)
+(if (executable-find "wakatime")
+		(setq wakatime-api-key "38377444-781c-4473-994e-1a3fca37ac3c")
+	(global-wakatime-mode)
+	)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
