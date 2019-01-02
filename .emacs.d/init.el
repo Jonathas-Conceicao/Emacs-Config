06e6d3671c69f93aa2ef1972cc340aaae3926fe2
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2019-01-02 16:48:23 -0300

    Config and gitignore update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,9 +15,6 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 
-;; Tab width
-(setq-default tab-width 2)
-
 ;; turn on highlighting current line
 (global-hl-line-mode 1)
 
@@ -34,45 +31,15 @@
 (savehist-mode 1)
 
 ;; auto insert closing bracket
-;; (electric-pair-mode 1)
+(electric-pair-mode t)
 
-;; Face to hilight function calls in dracula-theme
-;; (defface font-lock-function-call-face
-;; 	'((t :foreground "#50fa7b"
-;; 			 :weight bold
-;; 			 ))
-;; 	"Face for function calls."
-;; 	:group 'my-lang-mode )
-
-;; Set identention offset for java-mode
-(add-hook
- 'java-mode-hook
- (lambda ()
-	 (setq c-basic-offset 2)
-	 )
- )
-
-;; add hook for shell-mode
-(add-hook
- 'shell-mode-hook
- (lambda ()
-	 (lambda ()
-		 (setq-local sh-basic-offset 2)
-		 (setq-local tab-width 2)
-		 (setq-local indent-tabs-mode t)
-		 )
-	 )
- )
-
-;; add hook for Latex mode
-(add-hook
- 'LaTeX-mode-hook
- (lambda ()
-	 (lambda ()
-		 (setq indent-tabs-mode t)
-		 )
-	 )
- )
+;; Defalt tabs and offset spaces
+(setq-default c-offset 2)
+(setq-default c-basic-offset 2)
+(setq-default python-indent 2)
+(setq-default sh-basic-offset 2)
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode t)
 
 ;; Some extra hilight for C derivated modes
 (add-hook
@@ -86,7 +53,7 @@
 	 )
  )
 
-;; Define a new cmm-mode
+;; cmm-mode for ghc's cmm
 (define-derived-mode cmm-mode c-mode 
 	"cmm-mode" 
 	"A variant of C mode for edditing GHC's cmm (C--) files."
@@ -107,36 +74,27 @@
 (getenv "PATH")
 
 (defun move-line (n)
-  "Move the current line up or down by N lines."
-  (interactive "p")
-  (setq col (current-column))
-  (beginning-of-line) (setq start (point))
-  (end-of-line) (forward-char) (setq end (point))
-  (let ((line-text (delete-and-extract-region start end)))
-    (forward-line n)
-    (insert line-text)
-    ;; restore point to original column in moved line
-    (forward-line -1)
-    (forward-char col)))
+	"Move the current line up or down by N lines."
+	(interactive "p")
+	(setq col (current-column))
+	(beginning-of-line) (setq start (point))
+	(end-of-line) (forward-char) (setq end (point))
+	(let ((line-text (delete-and-extract-region start end)))
+		(forward-line n)
+		(insert line-text)
+		;; restore point to original column in moved line
+		(forward-line -1)
+		(forward-char col)))
 
 (defun move-line-up (n)
-  "Move the current line up by N lines."
-  (interactive "p")
-  (move-line (if (null n) -1 (- n))))
+	"Move the current line up by N lines."
+	(interactive "p")
+	(move-line (if (null n) -1 (- n))))
 
 (defun move-line-down (n)
-  "Move the current line down by N lines."
-  (interactive "p")
-  (move-line (if (null n) 1 n)))
-
-;; Fix colors on Shell
-(eval-after-load 'shell
-   '(progn
-      (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
-      (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on t)
-      t))
-(setq ansi-color-names-vector
-  ["black" "red" "green" "yellow" "PaleBlue" "magenta" "cyan" "white"])
+	"Move the current line down by N lines."
+	(interactive "p")
+	(move-line (if (null n) 1 n)))
 
 ;; My personal Key Bindings
 (global-set-key (kbd "<C-tab>") 'buffer-menu) ;; Buffer Menu
@@ -147,14 +105,10 @@
 
 ;; Habilitar acentos ortográficos
 (set-keyboard-coding-system 'utf-8)
-(require 'iso-transl)
+(when (require 'iso-transl nil :noerror) )
 
 (when (require 'whitespace nil :noerror)
-	;; Config for whitespace-mode
-	;; Make whitespace-mode with very basic background coloring for whitespaces.
-	;; Edited from: http://ergoemacs.org/emacs/whitespace-mode.html
 	(setq whitespace-style (quote (face spaces tabs newline space-mark tab-mark newline-mark )))
-	;; Make whitespace-mode and whitespace-newline-mode use “¬” for end of line char and “»” for tab.
 	(setq whitespace-display-mappings
 				;; all numbers are unicode codepoint in decimal. e.g. (insert-char 182 1)
 				'((space-mark 32 [183] [46]) ; SPACE 32 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
@@ -165,19 +119,13 @@
 	(set-face-attribute 'whitespace-space nil :background nil :foreground "gray40")
 	(set-face-attribute 'whitespace-tab nil :background nil :foreground "gray40")
 	(set-face-attribute 'whitespace-newline nil :background nil :foreground "gray40")
-	(global-whitespace-mode 1)
+	(global-whitespace-mode t)
 	)
 
 ;; The default theme
-
 (when (require 'dracula-theme nil :noerror)
 	(load-theme 'dracula t)
 	)
-
-;; Golang configs
-;; (require 'go-autocomplete)
-;; (require 'auto-complete-config)
-;; (add-to-list 'load-path "~/.emacs.d/golang/")
 
 ;; Runs go fmt after save
 (add-hook 'before-save-hook #'gofmt-before-save)
@@ -237,11 +185,6 @@
 							(setq-local indent-tabs-mode t)
 							)
 						)
-	)
-
-(if (executable-find "wakatime")
-		(setq wakatime-api-key "38377444-781c-4473-994e-1a3fca37ac3c")
-	(global-wakatime-mode)
 	)
 
 (custom-set-variables
