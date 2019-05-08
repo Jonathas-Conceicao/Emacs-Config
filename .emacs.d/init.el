8062f1934616ed5ca2124b0772dc24e9ff5cd1dc
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2019-05-08 16:10:55 -0300

    Removed marmalade package archive
    Signed-off-by: Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,6 @@
 (require 'package) 
 ;; Package Repositories
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
